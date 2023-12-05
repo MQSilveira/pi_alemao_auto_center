83bebffe5e777687c8630e319bf011f1aa200829
Marcos Silveira <marcosilveira.lg@gmail.com>
2023-12-05 12:45:33 -0300

    criação de 'fale conosco'

--- Frontend/src/screens/budget/Budget.jsx
+++ Frontend/src/screens/budget/Budget.jsx
@@ -1,13 +1,24 @@
+import './Budget.css'
+import * as Icons from '../../utils/IconsUtils'
+
 
 export default function Budget() {
     return (
-        <div className="budget">
-            <div className="budget-container">
-                <div className="budget-content">
-                    <h1 className="budget-title">Faça seu orçamento</h1>
-                    <h2 className="budget-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</h2>
-                    <p className="budget-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
-                    <a href="#appContact" className="budget-btn">Contato</a>
+        <div className='budget'>
+            <div className='budget-container'>
+                <div className='budget-content'>
+                    <h3>Fale Conosco</h3>
+                    <p>Estamos ansiosos para recebê-lo em nosso Auto Center, onde nos dedicamos a proporcionar os melhores serviços automotivos. Se precisar de um orçamento ou tiver alguma dúvida, não hesite em entrar em contato conosco pelo WhatsApp. Nossa equipe está pronta para atendê-lo com eficiência e profissionalismo. Aguardamos ansiosos pela oportunidade de servi-lo! </p>
+
+                    <a href="#appContact" className="aappContact">
+                        <i><Icons.WatsappIcon /></i>
+                        <span>Fale Conosco via WhatsApp</span>
+                    </a>
+                    
+                    {/*<button onClick={() => window.location.href = 'https://api.whatsapp.com/send?phone=SEUNUMERODOTELEFONE&text=Olá!%20Gostaria%20de%20um%20orçamento.'}>
+                        Fale Conosco no WhatsApp
+                    </button> */}
+                    
                 </div>
             </div>
         </div>
